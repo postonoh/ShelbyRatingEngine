5381f15a27e33cf0160e5ec2c39213f3a90c1744
postonoh <postonoh@att.net>
2020-07-22 15:17:48 -0400

    Add project files.

---
+++ README.md
@@ -0,0 +1,9 @@
+# SOLID Principles for C# Developers
+
+A sample application used to demonstrate SOLID principles for the Pluralsight course, [SOLID Principles for C# Developers](
+
+## Give a Star! :star:
+If you like this project please give it a star. Thanks!
+
+
+
---
+++ ShelbyRating/RaterFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ShelbyRating
+{
+    public class RaterFactory
+    {
+        public Rater Create(Policy policy, RatingEngine engine)
+        {
+            try
+            {
+                return (Rater)Activator.CreateInstance(
+                    Type.GetType($"ShelbyRating.{policy.Type}PolicyRater"),
+                        new object[] { engine, engine.Logger });
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
